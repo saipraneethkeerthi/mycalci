dbc73161be667d81e7db1478ab6a425ee1dd0051
Keerthi Sai Praneeth <kpraneeth@innominds.com>
2021-11-16 12:43:04 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
-import Signup from './components/test-components/globalcomponents/signup/signup'
-import Login from "./components/test-components/globalcomponents/login/login"
-import Landing from './components/test-components/globalcomponents/Landing'
-import Dashboard from './components/test-components/globalcomponents/dashboard/dashboard'
-import ErrorPage from './components/test-components/globalcomponents/404/404'
-import {useSelector} from "react-redux"
+import Body from "./components/Body"
+import TopBar from "./components/TopBar"
+import BottomBar from "./components/BottomBar"
 
 
 function App() {
-  const userData=useSelector((state)=>state.user)
-  // console.log("user......",userData)
-  const PageRender=(Page,auth)=>{
-    console.log(auth)
-    return <Page/>
-  }
+  // const userData=useSelector((state)=>state.user)
+  // // console.log("user......",userData)
+  // const PageRender=(Page,auth)=>{
+  //   console.log(auth)
+  //   return <Page/>
+  // }
   return (
     < >
-      <Router> 
+    <TopBar/>
+    <Body/>
+    <BottomBar/>
+      {/* <Router> 
         <Switch>
           <Route exact path="/register" render={()=>PageRender(Signup,'signup')}/> 
           <Route exact path="/login" render={()=>PageRender(Login,'login')}/> 
           <Route exact path="/" render={()=>PageRender(Landing,'landing')}/> 
           <Route exact path="/dashboard" render={()=>PageRender(Dashboard)}/> 
         </Switch>
-      </Router>
+      </Router> */}
        
     </>
   );
---
+++ src/components/TopBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Topbar = () => {
+    return (
+        <div>
+            <h1>top bar</h1>
+        </div>
+    );
+}
+
+export default Topbar;
