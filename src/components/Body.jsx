ed20ea49d355904ffc075fb489f582da70a7447c
jplox <rpawar@innominds.com>
2021-11-16 17:41:15 +0530

    rendering reusable component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -65,10 +65,22 @@ class Body extends Component {
       if(this.state.operator==="+"){
           this.setState({display2: this.state.finalOperand1+this.state.finalOperand2});
       }
+      else if(this.state.operator==="-"){
+        this.setState({display2: this.state.finalOperand1-this.state.finalOperand2})
+      }
+      else if(this.state.operator==="*"){
+        this.setState({display2: this.state.finalOperand1*this.state.finalOperand2})
+      }
+      else if(this.state.operator==="/"){
+        this.setState({display2: this.state.finalOperand1 / this.state.finalOperand2})
+      }else if(this.state.operator==="%"){
+        this.setState({display2: this.state.finalOperand1%this.state.finalOperand2})
+      }
   }
 
   render() {
     console.log(this.state);
+    const allVal=[{id:"operand",val:"7"}, {id:"operand", val:"8"},{id:"operand", val:"9"}, {id:"operator", val:"/"} , {id:"operand", val:"4"}, {id:"operand", val:"5"},{id:"operand", val:"6"}, {id:"operator", val:"*"}, {id:"operand", val:"1"}, {id:"operand", val:"2"}, {id:"operand", val:"3"}, {id:"operator", val:"-"}, {id:"operand", val:"0"}, {id:"operator", val:"."}, {id:"operator", val:"%"}, {id:"operator", val:"+"}]
     return (
       <>
         <h1>Calculator Design Using HTML Layout</h1>
