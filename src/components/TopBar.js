d0a0576513cbaef1b08f323acf2b94d1dc9dc8e1
Keerthi Sai Praneeth <kpraneeth@innominds.com>
2021-11-16 13:32:55 +0530

    added header (calculater state 23 in it) with comments

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,11 +1,18 @@
 import React,{useState} from 'react';
-
+/**
+ * @param {*} TopBar it will accept all the props and states
+ * @returns a html tags wihich has a state( count ) in it 
+ */
 const Topbar = () => {
+    /**
+     * @param {Integer} count 
+     * @param {*} setCount it is used to set state of the count
+     * count is the useState variable which will store the integer value in it
+     */
     const [count,setCount]=useState(23)
-
     return (
-        <div>
-            <h1>no of calculations:{count}</h1>
+        <div className=" card shadow">
+            <h2>no of calculations:{count}</h2>
         </div>
     );
 }
