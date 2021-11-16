52031fb546ab47ae429c764277bda58141e486c1
Vamshi Hari <vhari@innominds.com>
2021-11-16 13:48:00 +0530

    wrote TopBar and Bottombar in functional components and body in class components

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,14 +3,17 @@
  * The below code is used to display the body of the page
  *
  */
-import React from 'react';
+import React, { Component } from 'react';
 
-const Body = () => {
-    return (
-        <div>
-            <h1>body</h1>
-        </div>
-    );
+class Body extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
 }
 
 export default Body;
+
